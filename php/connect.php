1e72d9688412c5529c78415ad26606e791113781
Solomon <Moris@rpi.edu>
2017-10-15 19:20:26 -0400

    connect.php containts logic to manage JawsDB connection. Install.php has simple install for users table

---
+++ php/connect.php
@@ -0,0 +1,20 @@
+<?php
+$url = getenv('JAWSDB_MARIA_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/')
+
+try {
+    $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
